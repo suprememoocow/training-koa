9899f032ecae01f4a50313a86fe5279e17275917
Andrew Newdigate <andrew@troupe.co>
2014-06-28 15:44:03 +0100

    01, 02, 03 done

--- 03-routing/index.js
+++ 03-routing/index.js
@@ -3,14 +3,26 @@ var koa = require('koa');
 
 var app = module.exports = koa();
 
-app.use(function* () {
+app.use(function* (next) {
+  if(this.method !== 'GET' || this.path !== '/') {
+    return yield next;
+  }
 
+  this.response.body = 'hello world';
 });
 
-app.use(function* () {
+app.use(function* (next) {
+  if(this.method !== 'GET' || this.path !== '/404') {
+    return yield next;
+  }
 
+  this.response.body = 'page not found';
 });
 
-app.use(function* () {
+app.use(function* (next) {
+  if(this.method !== 'GET' || this.path !== '/500') {
+    return yield next;
+  }
 
+  this.response.body = 'internal server error';
 });
