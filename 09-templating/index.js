8a2933cabed2581f70228ea212588f85c84e9368
Andrew Newdigate <andrew@troupe.co>
2014-06-28 16:29:47 +0100

    Templates

--- 09-templating/index.js
+++ 09-templating/index.js
@@ -5,7 +5,17 @@ var path = require('path');
 
 var app = module.exports = koa();
 
+function render(filename) {
+  var self = this;
+  return function(done) {
+    jade.renderFile(filename, { }, done);
+  };
+}
+
 app.use(function* () {
   var filename = path.join(__dirname, 'homepage.jade');
-  // this.response.body=
+
+  var j = jade.compile(filename);
+
+  this.response.body = yield render(filename);
 });
