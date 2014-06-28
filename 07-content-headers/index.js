d53f2fb21659bfddc28b5269e68b3d764c8c4b91
Andrew Newdigate <andrew@troupe.co>
2014-06-28 16:14:39 +0100

    content neg

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -4,5 +4,9 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function* () {
-  
+  if(this.is('application/json')) {
+    this.body = {message: 'hi!'};
+  } else {
+    this.body = 'ok';
+  }
 })
